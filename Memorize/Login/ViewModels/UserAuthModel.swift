11447838ca114ecbc0fbc30a4ce5a8be51b00322
Wissal chebbi <chebbi.wissal@esprit.tn>
2023-01-01 11:16:49 +0100

    activate Google SignIn

--- Memorize/Login/ViewModels/UserAuthModel.swift
+++ Memorize/Login/ViewModels/UserAuthModel.swift
@@ -6,8 +6,8 @@
 //
 
 
-/*import SwiftUI
-import g
+import SwiftUI
+import GoogleSignIn
 
 class UserAuthModel: ObservableObject {
     
@@ -50,11 +50,16 @@ class UserAuthModel: ObservableObject {
         
        guard let presentingViewController = (UIApplication.shared.connectedScenes.first as? UIWindowScene)?.windows.first?.rootViewController else {return}
 
-        let signInConfig = GIDConfiguration.init(clientID: "CLIENT-ID")
+        let signInConfig = GIDConfiguration.init(clientID: "488385230841-oqb9oo72kl63d5df082qvt1oa3dhprpj.apps.googleusercontent.com")
         GIDSignIn.sharedInstance.signIn(
             with: signInConfig,
             presenting: presentingViewController,
             callback: { user, error in
+                print("Google Account: \(user?.profile?.name)")
+                print("Google Account: \(user?.profile?.email)")
+                print("Google Account: \(user?.profile?.hasImage)")
+                print("Google Account: \(user?.profile?.imageURL(withDimension: 32))")
+
                 if let error = error {
                     self.errorMessage = "error: \(error.localizedDescription)"
                 }
@@ -68,4 +73,4 @@ class UserAuthModel: ObservableObject {
         self.checkStatus()
     }
 }
-*/
+
